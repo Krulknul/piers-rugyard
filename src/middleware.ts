713192c4a6a0bb74f02fce1bc97e0b80c11e859a
Krulknul <85589076+Krulknul@users.noreply.github.com>
2025-03-13 20:27:51 +0100

    fix small bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,7 @@ export const adminCommand = async (ctx: MyContext, next: () => Promise<void>) =>
     }
     if (ctx.chat?.type === 'private') {
         await next();
+        return
     }
     if ((await ctx.getChatAdministrators()).find((admin) => admin.user.id === ctx.from!.id)) {
         await next();
